65701ce6f9a81467c2bd2d6b7e9d99ca530a1140
icefisher225 <ryan@cheevtech.com>
2022-04-08 12:09:11 -0400

    improving data structures, date/time handling

--- datastructures.py
+++ datastructures.py
@@ -1,22 +1,24 @@
-import os, sys, math, typing, datetime
+import os, sys, math, datetime, time
+
+from typing import Any, Union
 
 from helperfunctions import *
 
 
 class _Time:
     # Class to represent time. Called by _Time(Hour, Minute, Second)
-    def __init__(self, hour=0, minute=0, second=0):
+    def __init__(self, hour=0, minute=0, second=0) -> Union(None, Exception):
         if is_intable(hour, minute, second):
             pass
         else:
             return TypeError(
-                f"Require hour:int, minute:int, second:int. Got hour:{type(hour)}, minute:{type(minute)}, second:{type(second)}"
+                f"Require int, int, int. Got {type(hour)}, {type(minute)}, {type(second)}"
             )
         self._hour = int(hour) % 24
         self._minute = int(minute) % 60
         self._second = int(second) % 60
 
-    def pretty_print(self, *args, **kwargs):
+    def pretty_print(self, *args, **kwargs) -> str:
         hour, min = prt_conv_hlpr(self._hour), prt_conv_hlpr(self._minute)
         if self.second:
             return f"{hour}:{min}:{prt_conv_hlpr(self.second)}"
@@ -25,7 +27,9 @@ class _Time:
 
 
 class _Date:
-    def __init__(self, month=0, day=0, year=0, *args, **kwargs):
+    def __init__(
+        self, year=0, month=0, day=0, *args, **kwargs
+    ) -> Union(None, Exception):
         # Function returns if types are wrong, else returns nothing. Initializes an instance of a private "_Time" class.
         # This class should not be used out of this specific context.
         if (type(month) is int) and (type(day) is int) and (type(year) is int):
@@ -36,7 +40,7 @@ class _Date:
         self._month = month
         self._year = year
 
-    def _pp_year_conversion(self, *args, **kwargs):
+    def _pp_year_conversion(self, *args, **kwargs) -> str:
         # returns stringified year and converts to 20xx if just xx entered.
         if self._year > 99:
             return str(self._year)
@@ -44,7 +48,7 @@ class _Date:
             # catch if the year was entered as "22" instead of "2022" and fix
             return str(self._year + 2000)
 
-    def _pp_month_conversion(self, *args, **kwargs):
+    def _pp_month_conversion(self, *args, **kwargs) -> dict:
         conv_dict = {
             1: "jan",
             2: "feb",
@@ -61,16 +65,16 @@ class _Date:
         }
         return conv_dict[self._month]
 
-    def _pp_day_conversion(self, *args, **kwargs):
+    def _pp_day_conversion(self, *args, **kwargs) -> str:
         if self._day >= 10:
             return str(self._day)
         else:
             return "0" + str(self._day)
 
-    def __str__(self, *args, **kwargs):
+    def __str__(self, *args, **kwargs) -> str:
         return self.pretty_print(args, kwargs)
 
-    def pretty_print(self, *args, **kwargs):
+    def pretty_print(self, *args, **kwargs) -> str:
         # returns a date in format "daymonthyear" formatted like "02apr2022".
         return f"{self._pp_day_conversion()}{self._pp_month_conversion()}{self._pp_year_conversion()}"
 
@@ -78,12 +82,12 @@ class _Date:
 class _Source:
     def __init__(self, *args, **kwargs):
         """
-        Takes in the source of the signal as the first argument. All other arguments are not yet handled.
+        Takes in the source of the signal as the first argument.
         """
         if is_empty(args) and is_empty(kwargs):
-            self._source = "sumo"
+            self._source = "sysmon"
         else:
-            self._source = args[0]
+            self._source = str(args[0]).strip()
 
     def __str__(self, *args, **kwargs):
         return self.prettyprint(args, kwargs)
@@ -108,12 +112,11 @@ class Signal:
         self._details.append(str(info))
 
     def prettyprint(self, *args, **kwargs):
+        # Includes blank line after body
         description = ""
         for line in self._details:
-            description += f"{line}\n"
-        if self._atp == True:
-            return f"(ATP) {self._name}\n{description}"
-        return None
+            description += f"{line}\n\t"
+        return f"{str(self._name).capitalize()}\n\tSource: {str(self._source)}\n\t{description}\n"
 
     def __str__(self, *args, **kwargs):
         return self.prettyprint()
@@ -148,6 +151,9 @@ class Insight:
         self._number = number
         self._host = host
         self._user = user
+        self._date = None  # _Date class
+        self._time = None  # _Time class
+        self._activity_time = None  # _Time class
         self._datetime_dict = {
             "creation_hour": 0,
             "creation_minute": 0,
@@ -165,14 +171,26 @@ class Insight:
             self._entity = entity
         self._signals = (
             list()
-        )  # create empty list of signals - specific to Sumo Logic. Other SIEMs: use one signal per alert.
+        )  # create empty list of signals - specific to Sumo Logic/Exabeam(ish). Other SIEMs: one signal per alert.
 
-    def _handle_kwargs(self, *args, **kwargs):
+    def _set_creation_datetime(self, *args, **kwargs) -> None:
+        if not kwargs:
+            temp = time.localtime()()
+            self._date = _Date(temp[0], temp[1], temp[2])
+            self._time = _Time(temp[3], temp[4], temp[5])
         # TODO: check if this even works
-        for key in kwargs.keys:
-            dprint(f"{key}:{kwargs[key]}")
-            if key in self._datetime_dict.keys:
-                self._datetime_dict[key] = str(kwargs[key])
+        else:
+            for key in kwargs.keys:
+                dprint(f"{key}:{kwargs[key]}")
+                if key in self._datetime_dict.keys:
+                    self._datetime_dict[key] = str(kwargs[key])
 
-    def header_out(self):
+    def _header_out(self) -> str:
         return f"Insight {self._number}\n{self._time} {self._date}\nHost: {self._host}\nUser: {self._user}\nEntity: {self._entity}"
+
+    def _prettyprint(self) -> str:
+        return f"Insight-{self._number}\n{self._time} {self._date}"
+        pass
+
+    def __str__(self, *args, **kwargs) -> str:
+        return self._prettyprint()
